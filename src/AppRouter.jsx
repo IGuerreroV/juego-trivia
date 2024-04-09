435369384822a3eba48822bb43c8c524db9fa8d4
Iván Guerrero <ivangvprogrammer@gmail.com>
2024-04-08 23:39:56 -0500

    [ADD] Configuración de rutas con React Router

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,3 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import { HomePage, CategoryPage } from './pages'
+import { Navbar } from './components'
+
 export const AppRouter = () => {
-  return <div>AppRouter</div>
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/category/:category' element={<CategoryPage />} />
+      </Routes>
+    </>
+  )
 }
